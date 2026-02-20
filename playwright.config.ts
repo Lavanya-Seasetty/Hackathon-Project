b9c3c758b404ba887c99d030a2fb31ab304f43a0
Lavanya <seasettylavanya@gmail.com>
2026-02-20 11:53:07 +0530

    Commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
    }],
   ],
   timeout: 60*1000,
-  retries: 1,
+  retries: 2,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
